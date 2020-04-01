70ee073aabfe0f24902d321ffca500debca49062
Ryota <freehazamanet@gmail.com>
2020-04-01 11:09:07 +0000

    ログイン機能ログアウト機能追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    # 認証に成功した場合
+    if user&.authenticate(params[:session][:password])
+      # ログインする
+      log_in user
+      # フラッシュメッセージ
+      flash[:notice] = "#{user.name}でログインしました。"
+      # 特定のページにリダイレクトする
+      redirect_to('/')
+    else
+      # エラーメッセージの表示
+      flash.now[:danger] = 'メールアドレスもしくはパスワードが正しくありません。'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to ('/')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   get '/' => "home#top"
+  
+  # ユーザー
   get '/signup' => "users#new"
   resources :users
+  
+  # セッション
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 end
