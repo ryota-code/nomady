17cdcaff78401d84690993397f8d23934b1c0eda
Ryota <freehazamanet@gmail.com>
2020-04-01 03:10:52 +0000

    パスワード設定

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rails', '~> 5.2.4'
 gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
-
+gem 'bcrypt', '3.1.12'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL, message: ' は正しい形式で入力してください' }
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+end
