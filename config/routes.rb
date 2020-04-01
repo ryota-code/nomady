dffbeb944283301b17ed523b5f7214da03dfee97
Ryota <freehazamanet@gmail.com>
2020-04-01 02:43:02 +0000

    Delete User Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
   get '/' => "home#top"
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
-  } 
-  
-  devise_scope :user do
-    get "user/:id", :to => "users/registrations#detail"
-    get "signup", :to => "users/registrations#new"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
-  end
+  get '/signup' => "users#new"
 end
