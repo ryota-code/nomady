d523186cf22eb6e816c73643f034b1ba6f75b3dd
Ryota <freehazamanet@gmail.com>
2020-04-02 02:24:26 +0000

    サンプルユーザー追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  
+  def index
+    @user = User.all
+  end
+  
   def new
     @user = User.new
   end
@@ -40,5 +46,11 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  
+    
+    def logged_in_user
+      unless logged_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to login_url
+      end
+    end
 end
