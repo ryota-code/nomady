499cf5463805f1c45ffb86ff6881f6c994fdf157
Ryota <freehazamanet@gmail.com>
2020-04-10 00:26:37 +0000

    [Fix]Change application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,7 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
-//= require jquery.jpostal 
+//= require turbolinks
+//= require_tree .
