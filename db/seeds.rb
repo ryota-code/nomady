08d74487c124ec2817c61a0f55e00b81349484bd
Ryota <freehazamanet@gmail.com>
2020-04-24 01:05:11 +0000

    [Add]install tagsinput

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 
-User.create!(name: "加藤 浩輔",
+User.create!(name: "ミューツー",
             email: "example@nomady.com",
-            detail: "4年生/文学部/TOEIC800点/留学経験あり",
-            password: "admin1234",
-            password_confirmation: "admin1234",
+            detail: "技を受けた時、技のPPを2減らす。／レベルの高い野生のポケモンと出会いやすくなる。",
+            password: "admin",
+            password_confirmation: "admin",
             admin: true)
 
 
 20.times do |n|
-  name = Faker::Name.name
+  name = Faker::Pokemon.name
   email = "example-#{n+1}@nomady.com"
-  password = "admin1234"
-  detail = "4年生/文学部/TOEIC800点/留学経験あり"
+  password = "admin"
+  detail = "#{n+1}番目に捕まえられたポケモンです"
   User.create!(name:  name,
               email:  email,
               detail: detail,
@@ -21,12 +21,15 @@ User.create!(name: "加藤 浩輔",
 end
 
 users = User.order(:created_at).take(6)
-5.times do
-  title = Faker::Lorem.sentence(2)
+3.times do
+  title = "スターバックス"
   content = Faker::Lorem.sentence(5)
+  postcode =  Faker::Address.postcode
+  address_all = Faker::Address.state + Faker::Address.city + Faker::Address.street_name + Faker::Address.street_address
+  tag_list = "席いっぱい,コーヒー美味しい"
   wifi = "1"
   power = "1"
-  users.each { |user| user.cafeposts.create!(title: title, content: content, wifi: wifi, power: power) }
+  users.each { |user| user.cafeposts.create!(title: title, content: content, wifi: wifi, power: power, postcode: postcode, address_all: address_all, tag_list: tag_list ) }
 end
 
 
