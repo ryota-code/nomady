fa648c26b336b20a2afcb75fb9775bb9e171f2fd
Ryota <freehazamanet@gmail.com>
2020-04-27 02:25:50 +0000

    [Add]Dropdown menu

---
+++ app/assets/javascripts/dropdown.js
@@ -0,0 +1,29 @@
+$(function(){
+      // ドロップダウンメニュー
+  $('.dropdown-btn').hover(
+    function() {
+      //カーソルが重なった時
+      $(this).children('.dropdown').addClass('open');
+    }, function() {
+      //カーソルが離れた時
+      $(this).children('.dropdown').removeClass('open');
+    }
+  );
+   
+  // グローバルナビの開閉
+  $(function() {
+    $('.nav-button-wrap').on('click', function() {
+      if ($(this).hasClass('active')) {
+        // スマホ用メニューが表示されていたとき
+        $(this).removeClass('active');
+        $('.globalnav').addClass('close');
+        $('.globalnav-wrap , body').removeClass('open');
+      } else {
+        // スマホ用メニューが非表示の時
+        $(this).addClass('active');
+        $('.globalnav').removeClass('close');
+        $('.globalnav-wrap , body').addClass('open');
+      }
+    });
+  });
+});
