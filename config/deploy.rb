10fddfe16a5f9b3c83177a46c706cf722769c3e2
Ryota <freehazamanet@gmail.com>
2020-05-21 03:13:50 +0000

    [Update]deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,6 +55,17 @@ namespace :deploy do
       end
     end
   end
+  
+  desc 'db_seed'
+  task :db_seed do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
 
   after :publishing, :restart
 
