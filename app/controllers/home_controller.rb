a4a9a46eaeceed4bbb15b027053b45a95fb224ce
Ryota <freehazamanet@gmail.com>
2020-05-27 10:03:08 +0000

    [Update]toppage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def top 
-    
+    @q = Cafepost.ransack(params[:q])
+    @cafeposts = @q.result(distinct: true).order(" created_at DESC ").page(params[:page]).per(6)
+    @favorite_ranks = Cafepost.find(Favorite.group(:cafepost_id).order('count(cafepost_id) desc').limit(6).pluck(:cafepost_id))
   end
 end
